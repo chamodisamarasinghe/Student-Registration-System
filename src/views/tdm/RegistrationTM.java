81038567edb6ee8d85e6e3b058d05cce2f37dd39
chamodisamarasinghe <chamodisandunika98@gmail.com>
2022-01-02 10:24:38 +0530

    Update RegistrationTM

--- src/views/tdm/RegistrationTM.java
+++ src/views/tdm/RegistrationTM.java
@@ -3,19 +3,28 @@ package views.tdm;
 public class RegistrationTM {
     private String registerId;
     private String studentId;
-    private String registerDate;
-    private String time;
+    private String programmeId;
+    private String studentName;
+    private String address;
+    private String programme;
+    private String duration;
     private double payment;
+
 
     public RegistrationTM() {
     }
 
-    public RegistrationTM(String registerId, String studentId, String registerDate, String time, double payment) {
+    public RegistrationTM(String registerId, String studentId, String programmeId, String studentName,
+                          String address, String programme, String duration, double payment) {
         this.setRegisterId(registerId);
         this.setStudentId(studentId);
-        this.setRegisterDate(registerDate);
-        this.setTime(time);
+        this.setProgrammeId(programmeId);
+        this.setStudentName(studentName);
+        this.setAddress(address);
+        this.setProgramme(programme);
+        this.setDuration(duration);
         this.setPayment(payment);
+
     }
 
     public String getRegisterId() {
@@ -34,20 +43,44 @@ public class RegistrationTM {
         this.studentId = studentId;
     }
 
-    public String getRegisterDate() {
-        return registerDate;
+    public String getProgrammeId() {
+        return programmeId;
     }
 
-    public void setRegisterDate(String registerDate) {
-        this.registerDate = registerDate;
+    public void setProgrammeId(String programmeId) {
+        this.programmeId = programmeId;
     }
 
-    public String getTime() {
-        return time;
+    public String getStudentName() {
+        return studentName;
     }
 
-    public void setTime(String time) {
-        this.time = time;
+    public void setStudentName(String studentName) {
+        this.studentName = studentName;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getProgramme() {
+        return programme;
+    }
+
+    public void setProgramme(String programme) {
+        this.programme = programme;
+    }
+
+    public String getDuration() {
+        return duration;
+    }
+
+    public void setDuration(String duration) {
+        this.duration = duration;
     }
 
     public double getPayment() {
@@ -58,13 +91,18 @@ public class RegistrationTM {
         this.payment = payment;
     }
 
+
+
     @Override
     public String toString() {
         return "RegistrationTM{" +
                 "registerId='" + registerId + '\'' +
                 ", studentId='" + studentId + '\'' +
-                ", registerDate='" + registerDate + '\'' +
-                ", time='" + time + '\'' +
+                ", programmeId='" + programmeId + '\'' +
+                ", studentName='" + studentName + '\'' +
+                ", address='" + address + '\'' +
+                ", programme='" + programme + '\'' +
+                ", duration='" + duration + '\'' +
                 ", payment=" + payment +
                 '}';
     }
