bd9a2dab0c9aaa4afa91f8772c749effdf91f38b
chamodisamarasinghe <chamodisandunika98@gmail.com>
2022-01-02 12:42:02 +0530

    Updated ProgrammeDAO

--- src/dao/custom/ProgrammeDAO.java
+++ src/dao/custom/ProgrammeDAO.java
@@ -3,6 +3,8 @@ package dao.custom;
 import dao.SuperDAO;
 import entity.Programme;
 
+import java.sql.SQLException;
+
 public interface ProgrammeDAO extends SuperDAO<Programme, String> {
-    boolean ifProgrammeExist(String programmeId);
+    boolean ifProgrammeExist(String programmeId) throws SQLException, ClassNotFoundException;
 }
