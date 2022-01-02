76b6a3862be7d90acc328aa2e212eb3f9a8775b7
chamodisamarasinghe <chamodisandunika98@gmail.com>
2022-01-02 10:20:44 +0530

    Update RegistrationDAO

--- src/dao/custom/RegistrationDAO.java
+++ src/dao/custom/RegistrationDAO.java
@@ -3,5 +3,9 @@ package dao.custom;
 import dao.SuperDAO;
 import entity.Registration;
 
+import java.sql.SQLException;
+
 public interface RegistrationDAO extends SuperDAO<Registration, String> {
+    boolean ifRegisterExist(String id) throws SQLException, ClassNotFoundException;
+    String generateNewRegisterId() throws SQLException, ClassNotFoundException;
 }
